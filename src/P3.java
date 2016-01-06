b49ea14a40c0044a2c9b83b1b9e1e205d3f548b9
Telahnus <quan.jordan@gmail.com>
2016-01-06 10:49:06 -0500

    prime factorization, i feel like there should be a way to skip nonprimes

---
+++ src/P3.java
@@ -0,0 +1,23 @@
+/*
+ * The prime factors of 13195 are 5, 7, 13 and 29.
+ * What is the largest prime factor of the number 600851475143 ?
+ */
+public class P3 {
+
+	private static long num = 600851475143L;
+
+	public static void main(String[] args) {
+		long cur = num;
+		for (int i = 2; i <= cur; i++) {
+			//System.out.print(i + ",");
+			while (cur % i == 0) {
+				cur /= i;
+				System.out.println("pf:" + i + ", cur:" + cur);
+			}
+			if (cur == 1) {
+				System.out.println("top pf:" + i);
+				break;
+			}
+		}
+	}
+}
