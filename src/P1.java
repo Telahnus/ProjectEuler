9780a8241e11defb132b1cf882fe266e0b00b2c1
Telahnus <quan.jordan@gmail.com>
2016-01-05 19:47:19 -0500

    P1 & P1 in java

--- src/P1.java
+++ src/P1.java
@@ -1,9 +1,20 @@
-
 public class P1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int count = 0;
+		for (int i = 3; i < 1000; i += 3) {
+			count += i;
+			//System.out.println(i);
+		}
+		for (int i = 5; i < 1000; i += 5) {
+			count += i;
+			//System.out.println(i);
+		}
+		for (int i = 15; i < 1000; i += 15) {
+			count -= i;
+			//System.out.println(i);
+		}
+		System.out.println(count);
 	}
 
 }
